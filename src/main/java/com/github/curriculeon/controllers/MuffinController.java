13215b0c775f691624885efc835866c7cd7d6163
Calvin Quinn <caoyuru03@gmail.com>
2020-09-01 17:19:49 -0400

    finished models,repo,service,controller

--- src/main/java/com/github/curriculeon/controllers/MuffinController.java
+++ src/main/java/com/github/curriculeon/controllers/MuffinController.java
@@ -2,12 +2,21 @@ package com.github.curriculeon.controllers;
 
 import com.github.curriculeon.models.Muffin;
 import com.github.curriculeon.services.MuffinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping("/muffins")
 public class MuffinController {
     private MuffinService service;
 
+    @Autowired
     public MuffinController(MuffinService service) {
         this.service = service;
     }
@@ -16,19 +25,23 @@ public class MuffinController {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
-    public ResponseEntity<Muffin> show(Long id) {
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Muffin> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
-    public ResponseEntity<Muffin> create(Muffin baker) {
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<Muffin> create(@RequestBody Muffin baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
-    public ResponseEntity<Muffin> update(Long id, Muffin baker) {
+    @RequestMapping(value="update/{id}", method=RequestMethod.PUT)
+    public ResponseEntity<Muffin> update(@PathVariable Long id, Muffin baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
-    public ResponseEntity<Boolean> destroy(Long id) {
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/github/curriculeon/services/BakerService.java
+++ src/main/java/com/github/curriculeon/services/BakerService.java
@@ -2,12 +2,39 @@ package com.github.curriculeon.services;
 
 import com.github.curriculeon.repositories.BakerRepository;
 import com.github.curriculeon.models.Baker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class BakerService {
+
     private BakerRepository repository;
 
+    @Autowired
     public BakerService(BakerRepository repository) {
         this.repository = repository;
+    }
+
+    @PostConstruct
+    public void setup() {
+        Baker baker1 = new Baker();
+        baker1.setName("Leon");
+        baker1.setSpecialty("Bread");
+        baker1.setEmployeeId("01");
+
+        Baker baker2 = new Baker();
+        baker2.setName("Haseeb");
+        baker2.setSpecialty("Bagal");
+        baker2.setEmployeeId("02");
+
+        repository.save(baker1);
+        repository.save(baker2);
     }
 
     public Iterable<Baker> index() {
@@ -32,5 +59,12 @@ public class BakerService {
     public Boolean delete(Long id) {
         repository.deleteById(id);
         return true;
+    }
+
+    public List<Baker> readAll() {
+        Iterable<Baker> personIterable = repository.findAll();
+        List<Baker> personList = new ArrayList<>();
+        personIterable.forEach(personList::add);
+        return personList;
     }
 }
